b3faf16bbfad74fe8c9000ec228c77b01057e8ea
Rulo Kobashikawa <akobashikawa@gmail.com>
2018-02-06 14:32:57 -0500

    Mock votes as positive

--- mocks.js
+++ mocks.js
@@ -2,15 +2,16 @@ const casual = require('casual');
 // https://github.com/boo1ean/casual
 
 const mocks = {
-    Post: () => ({
-        id: casual.integer(from = 1, to = 1000),
-        title: casual.title
-    }),
-    Author: () => ({
-        id: casual.integer(from = 1, to = 1000),
-        firstName: casual.first_name,
-        lastName: casual.last_name
-    })
+  Post: () => ({
+    id: casual.integer((from = 1), (to = 1000)),
+    title: casual.title,
+    votes: casual.integer((from = 1), (to = 100))
+  }),
+  Author: () => ({
+    id: casual.integer((from = 1), (to = 1000)),
+    firstName: casual.first_name,
+    lastName: casual.last_name
+  })
 };
 
 module.exports = mocks;
